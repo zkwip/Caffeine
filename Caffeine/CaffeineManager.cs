5970e818430acb3688ed4c567cc9c856e155dfbd
Joep Bernards <zkwip2@gmail.com>
2022-10-20 16:45:47 +0200

    Inlined the wave file

--- Caffeine/CaffeineManager.cs
+++ Caffeine/CaffeineManager.cs
@@ -4,14 +4,22 @@ using System.Threading;
 using H.NotifyIcon.Core;
 using System.Drawing;
 using System.Diagnostics;
+using System.IO;
 
 namespace Caffeine
 {
     public sealed class CaffeineManager
     {
         private const int TIME_INTERVAL = 2000;
-        private const string SOUND_LOCATION = "silence.wav";
         private const KeyboardHandler.KeyCode WAKE_KEY = KeyboardHandler.KeyCode.F15;
+
+        private byte[] SILENCE = { 
+            0x52, 0x49, 0x46, 0x46, 0x25, 0x00, 0x00, 0x00, 
+            0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20,
+            0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02, 0x00, 
+            0x00, 0x7D, 0x00, 0x00, 0x00, 0xF4, 0x01, 0x00, 
+            0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61, 
+            0x00, 0x00, 0x00, 0x00,  };
 
         private readonly SoundPlayer _player;
         private readonly TrayIconWithContextMenu _icon;
@@ -21,8 +29,7 @@ namespace Caffeine
             KeyboardEnabled = keyboard;
             SoundEnabled = sound;
             Active = false;
-
-            _player = new SoundPlayer(SOUND_LOCATION);
+            _player = new SoundPlayer(new MemoryStream(SILENCE));
             _icon = CreateTrayIcon();
         }
 
@@ -126,6 +133,7 @@ namespace Caffeine
             catch (Exception)
             {
                 Active = false;
+                throw;
             }
         }
 
